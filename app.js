9ea164bea8049bc5422adace29efc0576f3228e0
Ahmed Junaed Nur <91321087+ahmedjunaedn@users.noreply.github.com>
2022-02-20 13:54:16 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Hello! I'm Ahmed Junaed Nur. I'm Father of this website👨 Welcome to 'Hahaha' Hacking Zone.");
+alert("INSTRUCTION : AFTER YOU LOGGED IN, DOWNLOAD POPUP IS HERE. DOWNLOAD YOUR LOG IN STATUS FILE FROM THIS WEBSITE.");
+confirm("Do you want to continue this website?");
+alert("Alright! Let's go.");
